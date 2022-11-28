b41ca13398d90b9e3f71dfad56f6fe4e5e64f67c
Christopher Bennett <chris@east20thst.net>
2022-11-28 16:06:00 -0500

    Add get by name option.

--- src/skytools/__main__.py
+++ src/skytools/__main__.py
@@ -6,12 +6,18 @@ from astropy.coordinates import SkyCoord
 from .atlas import u2k_chart, sky_quad
 
 parser = argparse.ArgumentParser()
-parser.add_argument("ra", help="Right ascension in hms")
-parser.add_argument("dec", help="Declination in dms")
+parser.add_argument("-ra", help="Right ascension in hms")
+parser.add_argument("-dec", help="Declination in dms")
+parser.add_argument('-n', '--name', help="Get object by name")
 args = parser.parse_args()
 
-coords = args.ra + args.dec
-c = SkyCoord(coords, unit=(u.deg, u.hourangle))
+if args.name:
+    c = SkyCoord.from_name(args.name)
+else:
+    coords = args.ra + args.dec
+    c = SkyCoord(coords, unit=(u.deg, u.hourangle))
+
+
 ra = c.ra.to_string(unit='hour', precision=0, format='unicode') # type: ignore
 dec = c.dec.to_string(unit='deg', precision=0, alwayssign=True, # type: ignore
                       format='unicode')
